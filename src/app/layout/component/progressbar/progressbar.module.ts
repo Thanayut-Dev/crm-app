6d61c56d62c68afa39ba024f774b6b835fef9bbe
Your Name <thanayut.phi@outlook.com>
2021-01-19 15:51:33 +0700

    gen layout -layout1, component-progressbar

--- src/app/layout/component/progressbar/progressbar.module.ts
+++ src/app/layout/component/progressbar/progressbar.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProgressbarComponent } from './progressbar.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ProgressbarComponent],
   imports: [
     CommonModule
   ]
