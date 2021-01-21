547fe1179217fa10e03f8fcdb9540cd17b9ef428
Your Name <thanayut.phi@outlook.com>
2021-01-21 10:15:08 +0700

    assets/icon/images/backgrounds

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,40 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './modules/auth/auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule)
+  },
+  {
+    path: 'deals',
+    loadChildren: () => import('./modules/generic-master/generic-master.module').then(m => m.GenericMasterModule),
+    data: { layouts: ['deals'] },
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'contact',
+    loadChildren: () => import('./modules/generic-master/generic-master.module').then(m => m.GenericMasterModule),
+    data: { layouts: ['contact'] },
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'customer',
+    loadChildren: () => import('./modules/generic-master/generic-master.module').then(m => m.GenericMasterModule),
+    data: { layouts: ['customer'] },
+    canActivate: [AuthGuard]
+  },
+  // {
+  //   path: 'settings',
+  //   loadChildren: () => import('./modules/settings/settings.module').then(m => m.SettingsModule),
+  //   canActivate: [AuthGuard]
+  // },
+  {
+    path: '**',
+    redirectTo: 'deals'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { Layout1Module } from './layout/layout1/layout1.module';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { MAT_DATE_LOCALE } from '@angular/material/core';
+import { AppInterceptor } from './app.interceptor';
 
 @NgModule({
   declarations: [
@@ -19,7 +21,15 @@ import { HttpClientModule } from '@angular/common/http';
 
     Layout1Module
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: AppInterceptor, multi: true },
+    { provide: MAT_DATE_LOCALE, useValue: 'en-GB' }
+    /**
+     * เปิด ErrorHandler เมื่อ Deploy app ขึ้น Prroduction environment
+     * ปิด ErrorHandler เมืออยู่ระหว่าง Development เพื่อ debug error
+     */
+    // { provide: ErrorHandler, useClass: AppErrorHandler },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
