be9277d7bd928faa9d76b8dd7110efefefcc308b
Your Name <thanayut.phi@outlook.com>
2021-01-22 14:06:48 +0700

    gen left form

---
+++ src/app/modules/left-form/left-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-left-form',
+  templateUrl: './left-form.component.html',
+  styleUrls: ['./left-form.component.scss']
+})
+export class LeftFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/modules/left-form/left-form.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LeftFormComponent } from './left-form.component';
+
+
+
+@NgModule({
+  declarations: [LeftFormComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class LeftFormModule { }
