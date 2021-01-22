7c77a81756b28ecab45fc638c27be6eb19c0f6c6
Your Name <thanayut.phi@outlook.com>
2021-01-22 12:20:10 +0700

    gen component generic-detail

---
+++ src/app/modules/generic-detail/generic-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-generic-detail',
+  templateUrl: './generic-detail.component.html',
+  styleUrls: ['./generic-detail.component.scss']
+})
+export class GenericDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/modules/generic-detail/generic-detail.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { GenericDetailComponent } from './generic-detail.component';
+
+
+
+@NgModule({
+  declarations: [GenericDetailComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class GenericDetailModule { }
