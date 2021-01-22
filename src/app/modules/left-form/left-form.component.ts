fb80e8ab31d902d94d93bac342fa422009b3be29
Your Name <thanayut.phi@outlook.com>
2021-01-22 14:58:50 +0700

    generic-detail module service , left-form

--- src/app/modules/left-form/left-form.component.ts
+++ src/app/modules/left-form/left-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-left-form',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./left-form.component.scss']
 })
 export class LeftFormComponent implements OnInit {
+
+  @Input() model: any;
 
   constructor() { }
 
--- src/app/modules/left-form/left-form.module.ts
+++ src/app/modules/left-form/left-form.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LeftFormComponent } from './left-form.component';
+import { DealFormComponent } from './deal-form/deal-form.component';
+import { ContacFormComponent } from './contac-form/contac-form.component';
+import { CustomerFormComponent } from './customer-form/customer-form.component';
 
 
 
 @NgModule({
-  declarations: [LeftFormComponent],
+  declarations: [LeftFormComponent, DealFormComponent, ContacFormComponent, CustomerFormComponent],
   imports: [
     CommonModule
-  ]
+  ],
+  exports: [LeftFormComponent]
 })
 export class LeftFormModule { }
